1a702520582e26bea89445c9b1e748f4454d9461
Lasse Nielsen <me@lasse.rocks>
2015-09-19 03:19:47 +0200

    Added fix for split

--- servermgmt.sh
+++ servermgmt.sh
@@ -3,6 +3,7 @@ tmux new-session -d -s mgmt
 tmux splitw -v 
 tmux splitw -h 
 tmux splitw -h 
+tmux splitw -h 
 tmux send-keys -t 1 'dev' enter C-1
 tmux send-keys -t 2 'ukuk' enter C-1
 tmux send-keys -t 3 'debian' enter C-1
@@ -12,5 +13,5 @@ tmux send-keys -t 5 'swdev' enter C-1
 tmux select-layout tiled
 
 tmux set synchronize-panes on 
-tmux selectp -t 1
+tmux selectp -t 2
 tmux -2 attach-session -t mgmt
