6877e45a8ed4e2289641114f4341cc8a5f70fbee
Lasse Nielsen <me@lasse.rocks>
2015-12-07 17:07:49 +0100

    dual monitors

--- thinkpad/actions/thinkpad-dock.sh
+++ thinkpad/actions/thinkpad-dock.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 xrandr --output LVDS1 --off --output VGA1 --auto --primary
+xrandr --output HDMI3 --auto --left-of VGA1
--- thinkpad/actions/thinkpad-undock.sh
+++ thinkpad/actions/thinkpad-undock.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+xrandr --output HDMI3 --off
 xrandr --output VGA1 --off --output LVDS1 --auto --primary
